7b09952f466da4a9f8fbd40994f4ae5fb6d431dd
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-02-03 11:44:20 -0300

    exercise 2.11

--- Part2/notesinglepage/src/App.js
+++ Part2/notesinglepage/src/App.js
@@ -1,54 +1,67 @@
-import { useState } from 'react'
-import Note from "./components/Note"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+// axios.get("http://localhost:3001/notes").then((response) => {
+//   const notes = response.data;
+//   console.log(notes);
+// });
+
+// const promise2 = axios.get("http://localhost:3001/foobar");
+// console.log(promise2);
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-    return (
+  return (
+    <div>
+      <h1>Notes</h1>
       <div>
-        <h1>Notes</h1>
-        <div>
-          <button onClick={() => setShowAll(!showAll)}>
-            show {showAll ? 'important' : 'all' }
-          </button>
-        </div>      
-        <ul>
-          {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
-        <form onSubmit={addNote}>
-        <input
-            value={newNote}
-            onChange={handleNoteChange}
-          />
-          <button type="submit">save</button>
-        </form> 
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
       </div>
-    )
-}
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,29 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
-const App = ()  => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-123456', id: 1 },
-    { name: 'Ada Lovelace', number:'39-44-5323523', id: 2 },
-    { name: 'Dan Abromov', number:'12-43-23445', id: 3 },
-    { name: 'Mary Poppendieck', number:'39-236423122', id: 4 },
-  ]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [searchName, setSearchName] = useState('');
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const addName = (event) => {
-
     event.preventDefault();
 
     const nameLowerCase = newName.toLowerCase();
-    const nameExists = persons.find(obj => obj.name.toLowerCase() === nameLowerCase);
+    const nameExists = persons.find(
+      (obj) => obj.name.toLowerCase() === nameLowerCase
+    );
 
     if (nameExists) {
       alert(`${newName} is already added to phonebook`);
-      setNewName('');
+      setNewName("");
     } else {
       const personObject = {
         name: newName,
@@ -32,23 +40,23 @@ const App = ()  => {
       };
 
       setPersons(persons.concat(personObject));
-      setNewName('');
-      setNewNumber('');
+      setNewName("");
+      setNewNumber("");
     }
   };
 
   const handlePersonChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+    console.log(event.target.value);
+    setNewName(event.target.value);
   };
 
   const handlePersonNumberChange = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
     setNewNumber(event.target.value);
   };
 
   const handleSearchNameChange = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
     const searchTerm = event.target.value;
     setSearchName(searchTerm);
   };
@@ -61,22 +69,22 @@ const App = ()  => {
     <div>
       <h1>Phonebook</h1>
 
-      <Filter value={searchName} onChange={handleSearchNameChange}/>
+      <Filter value={searchName} onChange={handleSearchNameChange} />
 
       <h2>Add a new</h2>
       <PersonForm
-      addName={addName}
-      newName={newName}
-      handlePersonChange={handlePersonChange}
-      newNumber={newNumber}
-      handlePersonNumberChange={handlePersonNumberChange}
+        addName={addName}
+        newName={newName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handlePersonNumberChange={handlePersonNumberChange}
       />
 
       <h2>Numbers</h2>
 
       <Persons filteredPersons={filteredPersons} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
