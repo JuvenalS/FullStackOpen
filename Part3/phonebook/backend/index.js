38a4c4e7ee19b4be6718b862bb7b689ef0fabe33
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-04-23 13:51:00 -0300

    Saving data to MongoDB

--- Part3/phonebook/backend/index.js
+++ Part3/phonebook/backend/index.js
@@ -1,35 +1,12 @@
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
-const app = express();
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const Person = require("./models/person");
 
 app.use(express.static("dist"));
-app.use(express.json());
-app.use(cors());
 
 app.use(
   morgan(function (tokens, request, response) {
@@ -45,6 +22,19 @@ app.use(
     ].join(" ");
   })
 );
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(express.json());
+app.use(cors());
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -62,29 +52,26 @@ const getDataAndHourCurrent = () => {
   })`;
 };
 
-const randomGenerateId = () => {
-  min = Math.ceil(1);
-  max = Math.floor(1000000);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).send("Not Found");
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -96,44 +83,52 @@ app.get("/info", (request, response) => {
       <p>${localDateFormat}</p>`);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  const nameExist = persons.some((person) => person.name === body.name);
-
-  if (nameExist) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+  if (body.number === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  if (!body.name && !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: randomGenerateId(),
+  });
+
+  person.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    number: body.number,
   };
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- Part3/phonebook/frontend/README.md
+++ Part3/phonebook/frontend/README.md
@@ -1,5 +1,9 @@
 # React + Vite
 
+Online application link:
+
+- [phonebook](https://backend-persons-hfmq.onrender.com/)
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
