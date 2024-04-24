7f439605c989796e3d55c973f2c3a6669f12ae0f
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-04-24 13:28:17 -0300

    linting notes

--- Part3/notes/backend/mongo.js
+++ Part3/notes/backend/mongo.js
@@ -1,27 +1,27 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
   const noteSchema = new mongoose.Schema({
     content: String,
-    important: Boolean,
-  });
+    important: Boolean
+  })
 
-  const Note = mongoose.model("Note", noteSchema);
+  const Note = mongoose.model('Note', noteSchema)
 
   const note = new Note({
-    content: "HTML is x",
-    important: true,
-  });
+    content: 'HTML is x',
+    important: true
+  })
 
   /*
   note.save().then(result => {
@@ -31,8 +31,8 @@ mongoose.connect(url).then(() => {
   */
   Note.find({}).then((result) => {
     result.forEach((note) => {
-      console.log(note);
-    });
-    mongoose.connection.close();
-  });
-});
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+})
